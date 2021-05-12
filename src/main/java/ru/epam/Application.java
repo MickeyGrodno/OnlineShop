5919a3670483946ea82f8b5a60e2c4c7e3b9e06d
Sergei <sergei199@list.ru>
2021-05-12 17:08:31 +0300

    Подключен Spring boot

--- src/main/java/ru/epam/Application.java
+++ src/main/java/ru/epam/Application.java
@@ -1,15 +1,12 @@
 package ru.epam;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
 import ru.epam.dao.*;
 
-@EnableJpaRepositories
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
